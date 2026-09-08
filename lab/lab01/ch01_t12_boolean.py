7a477b12b4196185484bb98bcfea1b85e924e5af
WuAidan-2627 <aidanwu2003@gmail.com>
2026-09-08 02:40:06 +0000

    [skip actions] lab/lab01/ch01_t12_boolean.py

--- lab/lab01/ch01_t12_boolean.py
+++ lab/lab01/ch01_t12_boolean.py
@@ -1,8 +1,6 @@
 # Hi! I'm Maria and I live in script.py.
 # I'm an expert Python coder.
-# I'm 21 years old and I plan to program cool stuff forever.
-a = True
-b = False
+# I'm 21 years old and I plan to program cool stuff forever.\
 name_is_maria = 21
 age_is_12 = int(input)
 if name_is_maria ==12:
