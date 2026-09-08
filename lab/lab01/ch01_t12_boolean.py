c613b4311d1af9c5acdf3a849156de6ef2720cda
WuAidan-2627 <aidanwu2003@gmail.com>
2026-09-08 02:42:29 +0000

    [skip actions] lab/lab01/ch01_t12_boolean.py

--- lab/lab01/ch01_t12_boolean.py
+++ lab/lab01/ch01_t12_boolean.py
@@ -3,6 +3,6 @@
 # I'm 21 years old and I plan to program cool stuff forever.\
 name_is_maria = 21
 age_is_12 = name_is_maria
-if name_is_maria ==12:
+if name_is_maria == 12:
                True
 else:False
