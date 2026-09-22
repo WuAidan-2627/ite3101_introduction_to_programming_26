bd85631edef5a3299ba9897aaf8e0030048e6746
WuAidan-2627 <aidanwu2003@gmail.com>
2026-09-22 01:58:02 +0000

    lab/lab06/ch06_t03_call_n_response.py

--- lab/lab06/ch06_t03_call_n_response.py
+++ lab/lab06/ch06_t03_call_n_response.py
@@ -4,5 +4,7 @@ def square(n: float) -> float:
     print("%d squared is %d." % (n, squared))
     return squared
 
+
 # Call the square function on line 10! Make sure to
 # include the number 10 between the parentheses.
+square(10)
