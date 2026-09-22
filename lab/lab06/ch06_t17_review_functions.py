6222c4b12d2ff4b512659a40bd1d16b59b804f7c
WuAidan-2627 <aidanwu2003@gmail.com>
2026-09-22 03:04:00 +0000

    [skip actions] lab/lab06/ch06_t17_review_functions.py

--- lab/lab06/ch06_t17_review_functions.py
+++ lab/lab06/ch06_t17_review_functions.py
@@ -1,7 +1,7 @@
 def shutdown(s: str) -> str:
     if s == "yes":
       print("Shutting down");
-     elif s == "no":
+     elseif s == "no":
       print("Shutdown aborted")
      else: 
       print("Sorry")
