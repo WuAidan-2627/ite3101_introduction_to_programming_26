bb086f75877ce247022bf43fb68f84f752b2df4c
WuAidan-2627 <aidanwu2003@gmail.com>
2026-09-26 19:17:31 +0000

    [skip actions] lab/lab04/ch04_t15_big_if.py

--- lab/lab04/ch04_t15_big_if.py
+++ lab/lab04/ch04_t15_big_if.py
@@ -2,7 +2,7 @@
 def grade_converter(answer: int) -> str:
     if 90 <= answer:
         return "A"
-    elif (79 < answer) and  (answer< 90):
+    elif (80 <= answer) and  (answer< 90):
         return "B"
     elif None:
         return "C"
