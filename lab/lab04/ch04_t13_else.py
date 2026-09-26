1db020fea4fbad4ab7659ccf96c224646165234f
WuAidan-2627 <aidanwu2003@gmail.com>
2026-09-26 19:13:46 +0000

    lab/lab04/ch04_t13_else.py

--- lab/lab04/ch04_t13_else.py
+++ lab/lab04/ch04_t13_else.py
@@ -5,7 +5,7 @@ def black_knight() -> bool:
     if answer == "'Tis but a scratch!":
         return True
     else:
-        return  # Make sure this returns False
+        return  False
 
 
 def french_soldier() -> bool:
