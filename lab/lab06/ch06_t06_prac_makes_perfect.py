c96217a9f90f53ae27aa777b70e380bc580c918f
WuAidan-2627 <aidanwu2003@gmail.com>
2026-09-26 18:33:36 +0000

    [skip actions] lab/lab06/ch06_t06_prac_makes_perfect.py

--- lab/lab06/ch06_t06_prac_makes_perfect.py
+++ lab/lab06/ch06_t06_prac_makes_perfect.py
@@ -2,8 +2,8 @@ def cube(number: int) -> int:
     return number**3
 
 
-def by_three(number2: int) -> int:
-    if cube(number2) % 3 == 0:
+def by_three(number: int) -> int:
+    if cube(number) % 3 == 0:
         return
     else:
         return print("False")
